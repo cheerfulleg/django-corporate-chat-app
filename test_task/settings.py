9d37e4a195795dfcd2c63646f2636c3c1a829e7c
cheerfulleg <61907072+cheerfulleg@users.noreply.github.com>
2020-11-16 15:07:54 +0200

    update settings for heroku deployment

--- test_task/settings.py
+++ test_task/settings.py
@@ -78,8 +78,10 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
+            # local
             # "hosts": [('127.0.0.1', 6379)],
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            # heroku
+            "hosts": [os.environ.get('REDIS_URL')],
         },
     },
 }
