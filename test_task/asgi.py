f88d87f6f4e03c8d30e953bc17537654320a5326
cheerfulleg <61907072+cheerfulleg@users.noreply.github.com>
2020-11-16 14:07:14 +0200

    updating asgi configs

--- test_task/asgi.py
+++ test_task/asgi.py
@@ -12,6 +12,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_task.settings')
 django.setup()
 application = get_default_application()
